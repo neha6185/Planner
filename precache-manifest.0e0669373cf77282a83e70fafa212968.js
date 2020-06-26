54787dee491dfa9f324431b009b359735b225126
neha6185 <neha.6185@gmail.com>
2020-06-26 00:46:54 -0700

    Updates

---
+++ precache-manifest.0e0669373cf77282a83e70fafa212968.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e414938ddcdbee6e4e890bb34c7c7386",
+    "url": "/Planner/index.html"
+  },
+  {
+    "revision": "3291695afd28f2168490",
+    "url": "/Planner/static/css/2.f2f930cc.chunk.css"
+  },
+  {
+    "revision": "85152879d78d5a045013",
+    "url": "/Planner/static/css/main.ddbfb4b1.chunk.css"
+  },
+  {
+    "revision": "3291695afd28f2168490",
+    "url": "/Planner/static/js/2.727a91c7.chunk.js"
+  },
+  {
+    "revision": "f6a5ed170d28ec77fdeb2985d7147407",
+    "url": "/Planner/static/js/2.727a91c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85152879d78d5a045013",
+    "url": "/Planner/static/js/main.7908aef1.chunk.js"
+  },
+  {
+    "revision": "94249fcaadb4c463042a",
+    "url": "/Planner/static/js/runtime-main.e662d65d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Planner/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Planner/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Planner/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Planner/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Planner/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
